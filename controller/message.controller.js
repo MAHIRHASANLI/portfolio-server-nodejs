635ecaadd424098fd2f52296b313bd19ab5e9f2f
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-10-28 11:55:31 +0400

    added nodemaile

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,4 +1,6 @@
 const MyMessageModel = require("../model/message.model");
+const nodemailer = require("nodemailer");
+
 
 const MessageController = {
     GetAll: async (req, res) => {
@@ -6,14 +8,59 @@ const MessageController = {
         res.status(200).send(GetAllModel);
     },
     Post: async (req, res) => {
+
+        const userEmail = req.body.email;
+        const userMessage = req.body.message;
+        const userName = req.body.name;
+        const userSurName = req.body.surname;
+
         const NewObj = new MyMessageModel({
-            name: req.body.name,
-            surname: req.body.surname,
-            email: req.body.email,
-            message: req.body.message
+            name: userName,
+            surname: userSurName,
+            email: userEmail,
+            message: userMessage
         });
         const newObj = await NewObj.save();
         res.status(200).send(newObj);
+
+        // Transporter yaradın
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'hasanlimahir1@gmail.com', // Sizin Gmail adresiniz
+                pass: 'zvbiwztyiuwxqzqq', // Yaratdığınız App Password
+            },
+        });
+
+        // Email göndərmə funksiyası
+        const mailMessage = {
+            from: {
+                name: `${userName} ${userSurName}`,
+                address: userEmail
+            }, // Göndərən adresi istifadəçinin emaili
+            to: "hasanlimahir1@gmail.com",
+            subject: "Message from the Portfolio website.",
+            html: `
+            <div>
+            <div><b>Name:</b>  ${userName}</div>
+            <div><b>Surname:</b>  ${userSurName}</div>
+            <div><b>Gmail:</b> ${userEmail}</div>
+            <hr style="margin:12px 0;"/>
+            <div>${userMessage}</div>
+            </div>`
+        };
+
+        // Emaili göndərmək
+        transporter.sendMail(mailMessage, (error, info) => {
+            if (error) {
+                console.log('Xəta baş verdi:', error);
+            } else {
+                console.log('Email uğurla göndərildi: ' + info.response);
+            }
+        });
     },
     Put: async (req, res) => {
         const newObj = {
--- validation/message.validation.js
+++ validation/message.validation.js
@@ -4,7 +4,7 @@ const MessageSchema = Joi.object().keys({
     name: Joi.string().min(4).max(30).required(),
     surname: Joi.string().min(4).max(30).required(),
     email: Joi.string().email().required(),
-    message: Joi.string().min(10).max(300).required()
+    message: Joi.string().min(5).max(300).required()
 });
 
 module.exports = MessageSchema;
